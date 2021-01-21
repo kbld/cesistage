700ce18908dabe16991fa3a5b41c9ccc556ec000
Eragon <sam.vzh@netc.fr>
2021-01-21 14:33:39 +0100

    Bases det templates twig

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require_once '/èvendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('views');
+$twig = new \Twig\Environment($loader, [
+    'cache' => '/path/to/compilation_cache',
+]);
