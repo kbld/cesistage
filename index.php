188b054397927fba12ba586d3c8b3b67897fbcd9
Eragon <sam.vzh@netc.fr>
2021-01-21 15:56:04 +0100

    Separating the css

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('views');
-$twig = new \Twig\Environment($loader, [
-    'cache' => '/tmp',
-]);
+$twig = new \Twig\Environment($loader, []);
 
 echo $twig->render('main.twig', []);
