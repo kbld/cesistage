0cc165c47f6cb6b3cd271c6f361f9d674813f0b1
Eragon <sam.vzh@netc.fr>
2021-01-21 16:17:54 +0100

    Playing with twig and the favoris page

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('views');
 $twig = new \Twig\Environment($loader, []);
 
-echo $twig->render('main.twig', []);
+echo $twig->render('favoris.twig', []);
