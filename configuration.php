801ddb018f2c979706ab2d51f0e27351abf19c5f
Eragon <sam.vzh@netc.fr>
2021-01-22 14:33:15 +0100

    Base du Register

--- configuration.php
+++ configuration.php
@@ -4,3 +4,5 @@ define('DATABASE_LOCATION', '127.0.0.1');
 define('DATABASE_NAME', 'cesistage');
 define('DATABASE_USER', 'cesi');
 define('DATABASE_PASSWORD', 'CesiPassWord');
+
+define('DATABASE_URL', 'mysql:hostname=' + DATABASE_LOCATION + ';dbname=' + DATABASE_NAME);
