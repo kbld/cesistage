61ca4fc9decb8e0c8e3c38679dc9406b6d86bd9f
Eragon <sam.vzh@netc.fr>
2021-01-22 14:33:15 +0100

    Base du Register

--- database.php
+++ database.php
@@ -1,2 +1,63 @@
 <?php
-new PDO('mysql:hostname=127.0.0.1;dbname=cesistage', 'cesi', 'CesiPassWord');
+require('configuration.php');
+
+try {
+	$dbh = new PDO(
+		DATABASE_URL,
+		DATABASE_USER,
+		DATABASE_PASSWORD,
+		array(
+			PDO::ATTR_PERSISTENT => true
+		)
+	);
+	catch (Exception $e) {
+		die("Unable to connect: " . $e->getMessage());
+	}
+}
+
+public function Register($user) {
+	$pass = password_hash(
+		$user['password'],
+		PASSWORD_ARGON2I,
+		[
+			'memory_cost' => 2048,
+			'time_cost' => 4,
+			'threads' => 3
+		]
+	);
+
+	try {
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$dbh->beginTransaction();
+
+		$stmt = $dbh->prepare(
+			"INSERT INTO users 
+			(name, lastName, email, username, password, status, enabled, company, group)
+			VALUES
+			(:name, :lastName, :email, :username, :password, :status, :enabled, :company, :group)"
+		);
+		$stmt->bindParam(':name', $name);
+		$stmt->bindParam(':lastName', $lastname);
+		$stmt->bindParam(':email', $email);
+		$stmt->bindParam(':username', $username);
+		$stmt->bindParam(':password', $password);
+		$stmt->bindParam(':status', $status);
+		$stmt->bindParam(':enabled', 0);
+		$stmt->bindParam(':company', $company);
+		$stmt->bindParam(':group', $group);
+		$stmt->execute();
+
+		$dbh->commit();
+	} catch (Exception $e) {
+		$dbh->rollBack();
+		echo "Failed: " . $e->getMessage();
+	}
+}
+
+public function GetNumberOfOffers() {
+	# code...
+}
+
+public function GetNumberOfOffersOf($type) {
+	# code...
+}
---
+++ register.php
@@ -0,0 +1,73 @@
+<?php
+require('utils.php');
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('views');
+$twig = new \Twig\Environment($loader, []);
+
+$errors = array(
+	'nom' => '',
+	'prenom' => '',
+	'username' => '',
+	'email' => '',
+	'password' => '',
+	'password2' => ''
+);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["nom"])) {
+		$errors['nom'] = "Le nom est obligatoire";
+	}
+	else {
+		$name = CleanInput($_POST["nom"]);
+		if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+			$errors['nom'] = "Seule les lettres et les espaces sont autorisé";
+		}
+	}
+	if (empty($_POST["prenom"])) {
+		$errors['prenom'] = "Le prénom est obligatoire";
+	}
+	else {
+		$prenom = CleanInput($_POST["prenom"]);
+		if (!preg_match("/^[a-zA-Z-']*$/",$prenom)) {
+			$errors['prenom'] = "Seul les lettres sont autorisés";
+		}
+	}
+	if (empty($_POST["username"])) {
+		$errors['username'] = "Le prénom est obligatoire";
+	}
+	else {
+		$username = CleanInput($_POST["username"]);
+		if (!preg_match("/^[a-zA-Z-_0-9]*$/",$username)) {
+			$errors['username'] = "Seul les lettres, les chifres et les charactères _- sont autorisés";
+		}
+	}
+	if (empty($_POST["email"])) {
+		$errors['email'] = "L'adresse mail est obligatoire";
+	}
+	else {
+		$email = CleanInput($_POST["email"]);
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$errors['email'] = "Email invalide";
+		}
+	}
+	if (empty($_POST["password"])) {
+		$errors['password'] = "Veuillez renseigner un mot de passe";
+	}
+	else {
+		$password = CleanInput($_POST["password"]);
+	}
+	if (empty($_POST["password2"])) {
+		$errors['password2'] = "Veuillez renseigner un mot de passe";
+	}
+	else {
+		$password2 = CleanInput($_POST["password2"]);
+	}
+	if ($password != $password2) {
+		$errors['password2'] = $errors['password'] = "Les mots de passe ne sont pas identiques";
+	}
+	echo $twig->render('register.twig', ['errors' => $errors]);
+}
+else {
+	echo $twig->render('register.twig', []);
+}
