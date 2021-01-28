fb02a747ef1156dc1eba274a4958711cc4d39146
Eragon <sam.vzh@netc.fr>
2021-01-28 16:53:38 +0100

    Counting offers

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 require_once 'vendor/autoload.php';
+require 'database.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('views');
 $twig = new \Twig\Environment($loader, []);
 
-echo $twig->render('main.twig', []);
+$numbers_of_offers = GetNumberOfOffers();
+
+echo $twig->render('main.twig', ['offers' => $numbers_of_offers]);
