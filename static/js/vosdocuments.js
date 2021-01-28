ce7c220491beb4a6fbb3294c630eb4a06295fbcc
Kbld <bk.killianb@gmail.com>
2021-01-28 10:10:15 +0100

    killian-correctionbug

--- static/js/vosdocuments.js
+++ static/js/vosdocuments.js
@@ -1,4 +1,4 @@
-var checkbox = document.querySelector( '#myInput' );
+/*var checkbox = document.querySelector( '#myInput' );
 var icon = document.querySelector( '#menuToggle span' );
 var listener = function( e ) {
   if( e.target != checkbox && e.target != icon ) {
@@ -11,4 +11,4 @@ checkbox.addEventListener( 'click', function(){
   if( this.checked ) {
     document.addEventListener( 'click', listener );
   } 
-});
+});*/
