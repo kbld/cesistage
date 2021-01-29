c68254b521278b512f9f670d70ef8370c7dafa6a
Eragon <sam.vzh@netc.fr>
2021-01-29 15:45:21 +0100

    Responding if the search return nothing

--- offers.php
+++ offers.php
@@ -13,21 +13,22 @@ if ((isset($_GET['search']) && $_GET['search'] != '') && (isset($_GET['limit']) 
 	$offers_number = GetNumberOfOffers();
 	$offers = SearchOffers($search, $limit);
 
-	echo $twig->render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers]);
+	echo $twig->render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'], 'match' => $offers['affected']]);
 } elseif (isset($_GET['search']) && $_GET['search'] != '') {
 	$search = CleanInput($_GET['search']);
+	$offers_number = GetNumberOfOffers();
 	$offers = SearchOffers($search);
 
-	echo $twig->render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers]);
+	echo $twig->render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'], 'match' => $offers['affected']]);
 } elseif (isset($_GET['limit']) && $_GET['limit'] != '') {
 	$limit = CleanInput($_GET['limit']);
 	$offers_number = GetNumberOfOffers();
 	$offers = GetOffers($limit);
 
-	echo $twig->render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers]);
+	echo $twig->render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'], 'match' => $offers['affected']]);
 } else {
 	$offers_number = GetNumberOfOffers();
 	$offers = GetOffers();
 
-	echo $twig->render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers]);
+	echo $twig->render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'], 'match' => $offers['affected']]);
 }
