0136146e715cdadfe3ed8a57c1291f017860493f
Eragon <sam.vzh@netc.fr>
2021-02-02 13:44:48 +0100

    Update

--- offers.php
+++ offers.php
@@ -2,28 +2,7 @@
 require_once 'database.php';
 require_once 'utils.php';
 
-if ((isset($_GET['search']) && $_GET['search'] != '') && (isset($_GET['limit']) && $_GET['limit'] != '')) {
-	$search = CleanInput($_GET['search']);
-	$limit = CleanInput($_GET['limit']);
-	$offers_number = GetNumberOfOffers();
-	$offers = SearchOffers($search, $limit);
+$offers_number = GetNumberOfOffers();
+$offers = GetOffers();
 
-	Render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'], 'match' => $offers['affected']]);
-} elseif (isset($_GET['search']) && $_GET['search'] != '') {
-	$search = CleanInput($_GET['search']);
-	$offers_number = GetNumberOfOffers();
-	$offers = SearchOffers($search);
-
-	Render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'], 'match' => $offers['affected']]);
-} elseif (isset($_GET['limit']) && $_GET['limit'] != '') {
-	$limit = CleanInput($_GET['limit']);
-	$offers_number = GetNumberOfOffers();
-	$offers = GetOffers($limit);
-
-	Render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'], 'match' => $offers['affected']]);
-} else {
-	$offers_number = GetNumberOfOffers();
-	$offers = GetOffers();
-
-	Rrender('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'], 'match' => $offers['affected']]);
-}
+Render('offers.twig', ['offers_number' => $offers_number, 'offers' => $offers['request'] ?? 0, 'match' => $offers['affected'] ?? 0]);
