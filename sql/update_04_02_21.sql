b647b5cb58fb3bb7c5d100626f3ff1968d804827
Eragon <sam.vzh@netc.fr>
2021-02-10 15:05:03 +0100

    Remove old SQLs files

--- sql/update_04_02_21.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `users`
-ADD `ProfilePicture` varchar(255) NULL,
-ADD `Description` varchar(255) COLLATE 'utf8mb4_unicode_ci' NULL;
--- sql/update_29_01_21.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `offers`
-ADD `description` text NOT NULL;
-ALTER TABLE `offers`
-ADD `title` varchar(255) COLLATE 'utf8mb4_unicode_ci' NOT NULL;
